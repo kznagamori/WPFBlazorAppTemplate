bbc7c8a716a31eff0ffe7f89a9efe6da54bfd2f0
kznagamori <kazuyuki.nagamori@gmail.com>
2024-08-08 12:51:54 +0900

    Pagesを分離

---
+++ Components/Pages/Counter.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ___PROJECTNAME___.Components.Pages {
+
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
